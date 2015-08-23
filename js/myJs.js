1ac5bfdb9faf4e2bfad59e8f06a5d3863582b864
dougdworkin <doug@dougdworkin.com>
2015-08-23 11:38:16 -0700

    fixed link to font

---
+++ js/myJs.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+	$('#clicker').click( function() {
+
+		$('#fader').slideUp(2000);
+	});
+
+
+
+		$('#unclicker').click( function() {
+
+		$('#fader').slideDown(2000);
+	});
+
+
+
+});
