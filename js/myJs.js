cb62886403e737e75f47d56a8ff97ba7ed03a1e1
dougdworkin <doug@dougdworkin.com>
2015-09-14 12:43:10 -0700

    updated site whil on vacation - added in monster graphics,chomp animation, darg and drop with plug in, message pop-up on email send, pront button functionality

--- js/myJs.js
+++ js/myJs.js
@@ -19,13 +19,15 @@ function addListItem() {
 		$('div.noItemText').remove();	
 		$('ul.listHolder').append(
 			'<li class="item, ui-state-default">' +
+			'<div class="teethUp"></div>' +
+			'<div class="teethDown"></div>' +
 			grippy +
 			'<div class="listItem">' +
 			'<label>' +
 			'<input type="checkbox" class="checkbox1" name="itemChecker">' +
-			'<span>' +
+			'<span><div>' +
 			text +
-			'</span>' +
+			'</div></span>' +
 			'</label>' +
 			'</div>' +
 			trashCan +
@@ -36,10 +38,11 @@ function addListItem() {
 		if ('$(#buttons).css("display","none")') {
 			$('#buttons').fadeIn(2000).css('display', 'block');
 		}
+		//adds top ul border if first list item
 		if($('ul.listHolder li').length >= 1){
 			$('ul.listHolder').css('borderTop','solid 2px #98cbe1');
 		}
-
+		//creates sortable list items using jquery ui	
 			$(function() {
   			  $( "#sortable" ).sortable();
   			  $( "#sortable" ).disableSelection();
@@ -62,7 +65,7 @@ function enterListItem(event) {
 function deleteItem () {
 	$(this).parent().slideUp(900, function () {
 		$(this).remove();
-		// remove ul bordertop and list buttons if no items
+		// remove ul bordertop and list buttons if no items left
 		if($('ul.listHolder li').length === 0){
 			$('ul.listHolder').css('borderTop','none');
 			$('div#buttons').fadeOut('slow');
@@ -82,14 +85,62 @@ function showEmailBox () {
 
 //close email box
 function closeEmailBox () {
-	$('#emailForm').slideUp('slow');
+	$('#emailForm').fadeOut('slow');
 	}
 
-function checkBox () {
-	//$(this).append('checked="checked"');
-	//alert('test');//
-}	
 
+// Runs animation, sound and crosses out/uncrosses on click
+
+function checkBox () {
+
+	var moveTeethUp = $(this).parent().parent().parent()
+	.find('div.teethUp');
+
+	var moveTeethDown = $(this).parent().parent().parent()
+	.find('div.teethDown');
+
+	var itemText = $(this).siblings('span').find('div');
+
+	var currentCheckBox = $(this)
+
+	function animateTeeth() {
+		moveTeethUp.addClass('animateIt');
+		moveTeethDown.addClass('animateIt');
+	}
+
+	animateTeeth();
+
+	// crosses out or uncrosses list item
+	setTimeout(function() {
+			if (currentCheckBox.prop('checked') == true) {
+				itemText.wrap('<s></s>');
+			} else {
+				itemText.unwrap();
+			}
+		}, 1000);
+
+	// removes the divs so checkbox is not blocked
+	setTimeout(function() {
+			moveTeethUp.removeClass('animateIt');
+	 		moveTeethDown.removeClass('animateIt');
+	 		}, 1500);
+
+}
+
+// removes the mail form on email button click, clears form, and shows message
+function showEmailSuccess() {
+	$('form#emailForm').fadeOut('fast');
+	$('div#successMessage').css('display','block').fadeIn('fast');
+
+	setTimeout(function() {
+			$('div#successMessage').fadeOut('slow');
+	 		}, 5500);
+	setTimeout(function() {
+		$('#emailForm .text').val('');
+	}, 400);
+
+}
+	
 
 
 $(document).ready(function(){
@@ -103,12 +154,37 @@ $('#text').on('keydown', enterListItem);
 //readies the document to run remove function on trashcan click
 $(document).on('click', 'input.delete', deleteItem)
 .on('click', 'div.noItemText, input#text',removeNoEnterBox)
+
 //readies the document to open and close email form
 .on('click', 'button#emailB', showEmailBox)
 .on('click', 'button.emailClose', closeEmailBox)
 
-.on('click', 'input.checkbox1', checkBox);
+//readies the document to run check functions when checkbox checked
+.on('click', 'input.checkbox1', checkBox)
+
+//gives email success message on button click
+.on ('click', 'input.sendEmail', showEmailSuccess)
 
 
+//print button
+.on ('click', 'button#printB', function() {
+	window.print();
+});
+
+// animates in side monsters
+$('div#monster01').animate({right:0}, 1000);
+$('div#monster02').animate({left:0}, 1400);
+
+
+// moves the eyes of the purple Monster
+$(".m1_pupilRight").jqEye({shape: "circle", radius:10});
+$(".m1_pupilMiddle").jqEye({shape: "circle", radius:10});
+$(".m1_pupilLeft").jqEye({shape: "circle", radius:10});
+
+// moves the eyes of the green monster
+$(".m2_pupilRight").jqEye({shape: "circle", radius:13});
+$(".m2_pupilLeft").jqEye({shape: "circle", radius:13});
 
 });
+
+
